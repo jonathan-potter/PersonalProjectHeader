19f13801a3c85a550e0fa72fee581c7e90654366
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-01-01 16:18:35 -0800

    adds actual project header related things

--- javascript/PersonalProjectHeader.jsx
+++ javascript/PersonalProjectHeader.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 export default React.createClass({
+  propTypes: {        
+    title: PropTypes.string,
+    demoUrl: PropTypes.string,
+    githubUrl: PropTypes.string,
+    wikipediaUrl: PropTypes.string
+  },
+
   render() {
     return (
-      <div>
-        <div>extra thing</div>
-        <div>extra thing</div>
-        <div>extra thing</div>
-        <div>extra thing</div>
-        <div>extra thing</div>
-        <div>extra thing</div>
-        <div>extra thing</div>
+      <div className='container'>
+        { this.renderTitle() }
+        { this.renderLinks() }
       </div>
+    )
+  },
+
+  renderTitle() {
+    return (
+      <span className='heading'>
+        <a href={this.props.demoUrl}>
+          {this.props.title}
+        </a>
+      </span>
+    )
+  },
+
+  renderLinks() {
+    return (
+      <nav className='right'>
+        <ul>
+          { this.renderGithubLink() }
+          { this.renderWikipediaLink() }
+        </ul>
+      </nav>
+    )
+  },
+
+  renderGithubLink() {
+    if (!this.props.githubUrl) { return }
+
+    return (
+      <li>
+        <a href={this.props.githubUrl} title="Github repo">
+          <i className='header-link icon-github-circled'></i>
+        </a>
+      </li>
+    )
+  },
+
+  renderWikipediaLink() {
+    if (!this.props.wikipediaUrl) { return }
+
+    return (
+      <li>
+        <a href={this.props.wikipediaUrl} title="Wikipedia">
+          <i className='header-link icon-wikipedia'></i>
+        </a>
+      </li>
     )
   }
 })
--- javascript/init.jsx
+++ javascript/init.jsx
@@ -3,23 +3,28 @@ import ReactDOM from 'react-dom';
 
 import PersonalProjectHeader from 'javascript/PersonalProjectHeader'
 
-// import 'dependencies/skeleton/css/normalize.css';
-// import 'dependencies/skeleton/css/skeleton.css';
-// import 'css/mandelbrot.css';
-// import 'css/header.css';
+import 'dependencies/skeleton/css/normalize.css';
+import 'dependencies/skeleton/css/skeleton.css';
+import 'css/header.css'
 
-let thing = React.createClass({
+const TITLE = 'Mandelbrot Set'
+const DEMO_URL = 'http://jonathan-potter.github.io/Mandelbrot'
+const GITHUB_URL = 'http://github.com/jonathan-potter/Mandelbrot'
+const WIKIPEDIA_URL = 'https://en.wikipedia.org/wiki/Mandelbrot_set'
+
+let projectHeader = React.createClass({
   render() {
     return (
-      <div>
-        <PersonalProjectHeader />
-        <div>another thing</div>
-      </div>
+      <PersonalProjectHeader
+        title={TITLE}
+        demoUrl={DEMO_URL}
+        githubUrl={GITHUB_URL}
+        wikipediaUrl={WIKIPEDIA_URL} />
     )
   }
 });
 
-let reactStuff = document.getElementById('react-stuff');
-ReactDOM.render(React.createElement(thing), reactStuff);
+let reactContainer = document.getElementById('personal-project-header');
+ReactDOM.render(React.createElement(projectHeader), reactContainer);
 
 
